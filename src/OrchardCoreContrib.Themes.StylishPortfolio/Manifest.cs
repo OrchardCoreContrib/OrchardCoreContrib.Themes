c5d52421c18f2703721ecd5613497c199f6e3731
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-01-31 21:46:16 +0300

    Add Stylish Portfolio theme

---
+++ src/OrchardCoreContrib.Themes.StylishPortfolio/Manifest.cs
@@ -0,0 +1,8 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Stylish Portfolio",
+    Author = "Orchard Core Contrib Team",
+    Version = "1.0.0",
+    Description = "A Free Bootstrap Theme by Start Bootstrap"
+)]
