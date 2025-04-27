fb54904b5120b4446dfa5a63f1000e14d6acef91
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-04-28 02:39:37 +0300

    Bump OCC.Themes.Resume 1.3.0

--- src/OrchardCoreContrib.Themes.Resume/Manifest.cs
+++ src/OrchardCoreContrib.Themes.Resume/Manifest.cs
@@ -3,6 +3,7 @@ using OrchardCore.DisplayManagement.Manifest;
 [assembly: Theme(
     Name = "Resume",
     Author = "Orchard Core Contrib Team",
-    Version = "1.2.0",
-    Description = "A clean, simple, yet elegant free resume/CV template for Bootstrap 5."
+    Version = "1.3.0",
+    Description = "A clean, simple, yet elegant free resume/CV template for Bootstrap 5.",
+    Dependencies = ["OrchardCore.Alias"]
 )]
